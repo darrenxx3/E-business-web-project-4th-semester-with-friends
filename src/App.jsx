7c6f6c853ff705e7d5f5d6800b6ec6305c233ec0
Obie <obiekrisnanto1@gmail.com>
2023-03-10 00:38:53 +0700

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { CssBaseline } from '@mui/material'
+import { GlobalStyles } from '@mui/styled-engine'
 import { useState } from 'react'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   
   return (
     <div className="App">
-      
+      <meta name="viewport" content="initial-scale=1, width=device-width" />
+      <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' } }} />
+      <CssBaseline />
+      <Navbar/>
     </div>
   )
 }
