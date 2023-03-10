3253e107765b4ca642c53f3d3a0fb97baf52a2bc
Obie <obiekrisnanto1@gmail.com>
2023-03-10 14:38:15 +0700

    flowbite

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
-import { CssBaseline } from '@mui/material'
-import { GlobalStyles } from '@mui/styled-engine'
 import { useState } from 'react'
 import './App.css'
+import CardItem from './components/CardItem'
 import Navbar from './components/Navbar'
 
 function App() {
   
   return (
     <div className="App">
-      <meta name="viewport" content="initial-scale=1, width=device-width" />
-      <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' } }} />
-      <CssBaseline />
-      <Navbar/>
+      
     </div>
   )
 }
