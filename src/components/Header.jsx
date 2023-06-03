11934195a22e2c51de7828a39f56efe8da60f364
Obie <obiekrisnanto1@gmail.com>
2023-06-03 17:19:10 +0700

    update nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,15 +10,15 @@ export default function Header() {
                 <div className="flex justify-between h-24">
                     <div className="flex-shrink-0 flex justify-items-center">
                         <Link to="/">
-                        <img className="block h-24 w-auto" src={logo} alt="Logo" />
+                            <img className="block h-24 w-auto" src={logo} alt="Logo" />
                         </Link>
                     </div>
                     <div className="bg-orange-400">
                         <ol className='flex h-full justify-center md:mx-16 gap-2 md:gap-14 align-middle'>
-                            <li className='my-auto text-xl font-bold rounded-full hover:bg-orange-200 p-2 transition-colors'><NavLink to="/">Home</NavLink></li>
-                            <li className='my-auto text-xl font-bold rounded-full hover:bg-orange-200 p-2 transition-colors'><NavLink to="/kendaraan">Kendaraan</NavLink></li>
-                            <li className='my-auto text-xl font-bold rounded-full hover:bg-orange-200 p-2 transition-colors'><NavLink to="/jadwal">Jadwal</NavLink></li>
-                            <li className='my-auto text-xl font-bold bg-blue-300 rounded-full py-2 px-4'><NavLink to="/kendaraan">Profile</NavLink></li>
+                            <NavLink className={"my-auto"} to="/"><li className='my-auto text-xl font-bold rounded-full hover:bg-orange-200 p-2 transition-colors'>Home</li></NavLink>
+                            <NavLink className={"my-auto"} to="/kendaraan"><li className='my-auto text-xl font-bold rounded-full hover:bg-orange-200 p-2 transition-colors'>Kendaraan</li></NavLink>
+                            <NavLink className={"my-auto"} to="/jadwal"><li className='my-auto text-xl font-bold rounded-full hover:bg-orange-200 p-2 transition-colors'>Jadwal</li></NavLink>
+                            <NavLink className={"my-auto"} to="/kendaraan"><li className='my-auto text-xl font-bold bg-blue-300 rounded-full py-2 px-4'>Profile</li></NavLink>
                         </ol>
                     </div>
                 </div>
